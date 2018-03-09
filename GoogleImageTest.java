088194aab3854467253f9b5cdc58b82487c1c132
Isaac Taylor <isaactay@usc.edu>
2018-03-09 02:25:30 -0800

    j

---
+++ GoogleImageTest.java
@@ -0,0 +1,25 @@
+
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class GoogleImageTest {
+
+	@Test
+	public void testGetSetImageType() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetSetUrlLink() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetSetImage() {
+		fail("Not yet implemented");
+	}
+
+
+}
